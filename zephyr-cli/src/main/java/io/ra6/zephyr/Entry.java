bbba28861bc27625b1e6368c007d0bfac5b1ca18
Rasixz | Michael <38357618+ra6z@users.noreply.github.com>
2023-04-15 06:45:14 +0200

    Changed stuff

--- zephyr-cli/src/main/java/io/ra6/zephyr/Entry.java
+++ zephyr-cli/src/main/java/io/ra6/zephyr/Entry.java
@@ -3,12 +3,10 @@ package io.ra6.zephyr;
 import picocli.CommandLine;
 
 public final class Entry {
-
     public static void main(String[] args) {
-        boolean verbose = false;
+//        boolean verbose = false;
 
-        args = "run D:\\Projects\\zephyr\\examples\\test.zph -t -std D:\\Projects\\zephyr\\stdlib".concat(verbose ? "-v" : "").split(" ");
-        //args = "run D:\\Projects\\zephyr\\std\\collections\\list.zph -v -t -std D:\\Projects\\zephyr\\std -- hello world".split(" ");
+//        args = "run D:\\Projects\\zephyr\\examples\\dictionary.zph -t -std D:\\Projects\\zephyr\\stdlib".concat(verbose ? "-v" : "").split(" ");
 
         int exitCode = new CommandLine(new Zephyr()).execute(args);
         System.exit(exitCode);
